dbf4b2dcd696bfb10cf791748a076480fc5dd7c0
Anael Madec <Anael.Madec@keyrus.com>
2025-01-24 09:54:32 +0100

    commit file mrt

--- models/mart/mrt_order_daily_report.sql
+++ models/mart/mrt_order_daily_report.sql
@@ -1,4 +1,5 @@
-SELECT DATE_TRUNC(order_created_at, DAY) AS reporting_date,
+SELECT 1 as column1,
+	DATE_TRUNC(order_created_at, DAY) AS reporting_date,
     mapping.account_manager,
     mapping.state,
     COUNT(DISTINCT order_id) AS total_orders,
