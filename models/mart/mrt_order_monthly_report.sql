92a840a3ea672e2fb3c6b4b7accc9a08f53710da
anael.madec@keyrus.com <anael.madec@keyrus.com>
2025-01-24 13:19:46 +0000

    merge subqueries

--- models/mart/mrt_order_monthly_report.sql
+++ models/mart/mrt_order_monthly_report.sql
@@ -1,24 +1,24 @@
 WITH monthly_users_recap AS (
 
-SELECT DATE_TRUNC(order_date,month) AS order_month,
-COUNT(DISTINCT user_name) AS total_monthly_users,
+SELECT DATE_TRUNC(order_created_at,month) AS order_month,
+COUNT(DISTINCT user_id) AS total_monthly_users,
   COUNT(order_id) AS total_monthly_orders
-FROM {{ source('sales_database', 'order')}}
+FROM {{ ref('stg_sales_database__order')}}
 GROUP BY order_month
 ORDER BY total_monthly_users DESC
 
 ), total_monthly_user_from_jawa_timur AS (
-SELECT DATE_TRUNC(order_date,month) AS order_month,
-COUNT(DISTINCT user.user_name) AS total_monthly_users_from_jawa_timur
-FROM {{ source('sales_database', 'order')}} AS orders
-LEFT JOIN {{ source('sales_database', 'user')}} AS user ON user.user_name = orders.user_name
-WHERE user.customer_state LIKE '%JAWA%TIMUR%'
+SELECT DATE_TRUNC(order_created_at,month) AS order_month,
+COUNT(DISTINCT user.user_id) AS total_monthly_users_from_jawa_timur
+FROM {{ ref('stg_sales_database__order')}} AS orders
+LEFT JOIN {{ ref('stg_sales_database__user')}} AS user ON user.user_id = orders.user_id
+WHERE user.user_state LIKE '%JAWA%TIMUR%'
 GROUP BY order_month
 )
 SELECT u.order_month,
 COALESCE(u.total_monthly_users,0) AS total_users_monthly,
 COALESCE(jt.total_monthly_users_from_jawa_timur,0) AS total_monthly_user_from_jawa_timer,
-COALESCE(o.total_monthly_orders,0) AS total_monthly_orders
+COALESCE(u.total_monthly_orders,0) AS total_monthly_orders
 FROM monthly_users_recap AS u
 LEFT JOIN total_monthly_user_from_jawa_timur AS jt ON jt.order_month = u.order_month
 ORDER BY order_month
