6d06eceef278d86cdeaba6ba3ca363b69b167b2d
anlmadec2 <anael.madec@keyrus.com>
2025-01-24 14:15:25 +0100

    Update and rename mrt_monthly_order_report.sql to mrt_order_monthly_report.sql

--- models/mart/mrt_order_monthly_report.sql
+++ models/mart/mrt_order_monthly_report.sql
@@ -1,12 +1,11 @@
 WITH monthly_users_recap AS (
 
-
 SELECT DATE_TRUNC(order_date,month) AS order_month,
-COUNT(DISTINCT user_name) AS total_monthly_users
+COUNT(DISTINCT user_name) AS total_monthly_users,
+  COUNT(order_id) AS total_monthly_orders
 FROM {{ source('sales_database', 'order')}}
-GROUP BY 1
+GROUP BY order_month
 ORDER BY total_monthly_users DESC
-
 
 ), total_monthly_user_from_jawa_timur AS (
 SELECT DATE_TRUNC(order_date,month) AS order_month,
@@ -15,17 +14,6 @@ FROM {{ source('sales_database', 'order')}} AS orders
 LEFT JOIN {{ source('sales_database', 'user')}} AS user ON user.user_name = orders.user_name
 WHERE user.customer_state LIKE '%JAWA%TIMUR%'
 GROUP BY order_month
-
-
-), monthly_orders_recap AS (
-
-
-SELECT DATE_TRUNC(order_date,month) AS order_month,
-COUNT(order_id) AS total_monthly_orders
-FROM {{ source('sales_database', 'order')}}
-GROUP BY order_month
-
-
 )
 SELECT u.order_month,
 COALESCE(u.total_monthly_users,0) AS total_users_monthly,
@@ -33,5 +21,4 @@ COALESCE(jt.total_monthly_users_from_jawa_timur,0) AS total_monthly_user_from_ja
 COALESCE(o.total_monthly_orders,0) AS total_monthly_orders
 FROM monthly_users_recap AS u
 LEFT JOIN total_monthly_user_from_jawa_timur AS jt ON jt.order_month = u.order_month
-LEFT JOIN monthly_orders_recap AS o ON o.order_month = u.order_month
 ORDER BY order_month
